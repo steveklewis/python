933256b2b1c8f9c9021fd12c0791402769ce5e86
Steve Lewis <spiritmech@gmail.com>
2013-06-01 22:25:54 -0400

    Using aglyph, both with XML and pure-Python configurations.

---
+++ aglyph/aglyph_tutorial/movielisterapp-basic/movies/__init__.py
@@ -0,0 +1,10 @@
+from aglyph.binder import Binder
+
+from movies.lister import MovieLister
+from movies.finder import MovieFinder, CSVMovieFinder
+
+class MoviesBinder(Binder):
+    def __init__(self):
+        super(MoviesBinder, self).__init__("movies-binder")
+        self.bind(MovieLister).init(MovieFinder)
+        self.bind(MovieFinder, to=CSVMovieFinder).init("movies.csv")
