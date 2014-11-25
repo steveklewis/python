8e9d4d00990c758ab82ca3a84d125c5fecddd90d
steveklewis <lewisksteven@gmail.com>
2014-11-25 18:53:41 -0500

    From norvig.com/lispy.html, evaluating the program with global environment.

--- lispy/lispy.py
+++ lispy/lispy.py
@@ -1,6 +1,62 @@
+Env = dict
 Symbol = str
 List = list
 Number = (int, float)
+
+def standard_env():
+    "An environment with some Scheme standard procedures."
+    import math, operator as op
+    env = Env()
+    env.update(vars(math))
+    env.update({
+        '+':op.add, '-':op.sub, '*':op.mul, '/':op.div,
+        '>':op.gt, '<':op.lt, '>=':op.ge, '<=':op.le, '=':op.eq,
+        'abs':    abs,
+        'append': op.add,
+        'apply': apply,
+        'begin': lambda *x: x[-1],
+        'car': lambda x: x[0],
+        'cdr': lambda x: x[1:],
+        'cons':    lambda x,y: [x] + y,
+        'eq?':     op.is_,
+        'equal?':  op.eq,
+        'length':  len,
+        'list':    lambda *x: list(x),
+        'list?':   lambda x: isinstance(x, list),
+        'map':     map,
+        'max':     max,
+        'min':     min,
+        'not':     op.not_,
+        'null?':    lambda x: x == [],
+        'number?':  lambda x: isinstance(x, Number),
+        'procedure?':  callable,
+        'round':    round,
+        'symbol?': lambda x: isinstance(x, Symbol)
+    })
+    return env
+
+global_env = standard_env()
+    
+def eval(x, env=global_env):
+    "Evaluate an expression in an environment."
+    if isinstance(x, Symbol):
+        return env[x]
+    elif not isinstance(x, List):
+        return x
+    elif x[0] == 'quote':
+        (_, exp) = x
+        return exp
+    elif x[0] == 'if':
+        (_, test, conseq, alt) = x
+        exp = (conseq if eval(test, env) else alt)
+        return eval(exp, env)
+    elif x[0] == 'define':
+        (_, var, exp) = x
+        env[var] = eval(exp, env)
+    else:
+        proc = eval(x[0], env)
+        args = [eval(arg, env) for arg in x[1:]]
+        return proc(*args)
 
 def atom(token):
     "Numbers become numbers; every other token is a symbol."
@@ -40,4 +96,8 @@ def parse(program):
 
 program = "(begin (define r 10) (* pi (* r r)))"
 
-print parse(program)
+#print eval(parse(program))
+
+## Set up the environment for r
+eval(parse("(define r 10)"))
+print eval(parse("(* pi (* r r))"))
