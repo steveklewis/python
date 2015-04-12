f5f352feaa8199c8e0b688e8b6d1c97539f72d71
steveklewis <lewisksteven@gmail.com>
2015-04-12 10:55:30 -0400

    Commenting out Django Rest Framework for now until I actually use it.

--- beaches/django_beaches/django_beaches/urls.py
+++ beaches/django_beaches/django_beaches/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
