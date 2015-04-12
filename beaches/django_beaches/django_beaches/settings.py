aeec155c9126d9f67b07e87e16f15cb334fa90b3
steveklewis <lewisksteven@gmail.com>
2015-04-12 11:32:58 -0400

    Got Django REST Framework going at a basic level.

--- beaches/django_beaches/django_beaches/settings.py
+++ beaches/django_beaches/django_beaches/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'rest_framework',
+    'rest_framework',
     'beaches_app',
 )
 
@@ -102,3 +102,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard 'django.contrib.auth' permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
